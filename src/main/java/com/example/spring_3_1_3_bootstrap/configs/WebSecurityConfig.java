78ee2fdf901a1cfc716e5efdfbf19f8676bf7cb8
Marat <hedarius1992@gmail.com>
2022-09-28 12:16:57 +0300

    version 1.1

--- src/main/java/com/example/spring_3_1_3_bootstrap/configs/WebSecurityConfig.java
+++ src/main/java/com/example/spring_3_1_3_bootstrap/configs/WebSecurityConfig.java
@@ -23,6 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable().antMatcher("/**")
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/admin-panel/**").hasRole("ADMIN")
